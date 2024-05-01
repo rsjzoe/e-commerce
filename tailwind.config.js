b60a7a8233764b3c7eb62258d6b5d0407db4f674
rsjzoe <rsjzoe@gmail.com>
2024-05-01 15:04:35 +0300

    timeline

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./main.js"
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "./main.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#E89651",
+      },
+    },
   },
   plugins: [],
-}
+};
